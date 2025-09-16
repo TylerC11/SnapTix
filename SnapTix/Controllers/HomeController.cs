40f2ce6cc33f03fb58dc76b25b95cb48eb0e5f0b
TylerC11 <w0432196@campus.nscc.ca>
2025-09-16 10:09:05 -0300

    Removed unnecessary files

--- SnapTix/Controllers/HomeController.cs
+++ SnapTix/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using SnapTix.Models;
 
 namespace SnapTix.Controllers
 {
@@ -23,10 +22,6 @@ namespace SnapTix.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+       
     }
 }
