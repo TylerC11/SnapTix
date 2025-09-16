2d8076c88ae12501e4ec709f5db04359b63eedce
TylerC11 <w0432196@campus.nscc.ca>
2025-09-16 10:13:27 -0300

    Added Models

---
+++ SnapTix/Models/Tag.cs
@@ -0,0 +1,14 @@
+﻿namespace SnapTix.Models
+{
+    public class Tag
+    {
+        public int TagId { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        // Foreign key to Ticket
+        public int PhotoId { get; set; }
+
+        // Navigation property (code)
+        public Ticket? Ticket { get; set; }
+    }
+}
---
+++ SnapTix/Models/Ticket.cs
@@ -0,0 +1,17 @@
+﻿namespace SnapTix.Models
+{
+    public class Ticket
+    {
+        // Primary key
+        public int TicketId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        // The Ticket's filename on the server
+        public string Owner { get; set; } = string.Empty;
+        public DateTime EventDate { get; set; }
+
+        // Navigation property (code)
+        public List<Tag>? Tags { get; set; }
+    }
+}
